58cc249a3d907ea40fdc85b084f51278f37c0fa0
Xcrid <36483768+Xcrid@users.noreply.github.com>
2019-04-24 01:51:16 +0200

    current python project
    One must install wiringpi on the raspberry.
    The code for the pwm can be changed from using gpio command in bash to the use of pigpio.
    this way :
    activate remote gpio in raspi-config then reboot, then launch the daemon with the sudo pigpiod command
    https://abyz.me.uk/rpi/pigpio/python.html#hardware_PWM

---
+++ src/pwm.py
@@ -0,0 +1,24 @@
+from subprocess import call
+from time import sleep
+
+def pwm_setup():
+
+	call('gpio mode 1 pwm', shell=True)
+	call('gpio mode 23 pwm', shell=True)
+	call('gpio pwm-ms', shell=True)
+	call('gpio pwmr 4000', shell=True)
+	call('gpio pwmc 77', shell=True)
+	call ('gpio pwm 1 400',shell=True)
+	call('gpio pwm 23 175',shell=True)
+	sleep(2)
+	return 0
+
+def pwm_motor(value):
+
+	call('gpio pwm 1 '+value,shell=True)
+	return 0
+	
+def pwm_turn(value):
+	
+	call('gpio pwm 23 '+value,shell=True)
+	return 0
---
+++ src/uart_raspberry.py
@@ -0,0 +1,34 @@
+import serial
+
+ser = serial.Serial(
+    port='/dev/ttyAMA0',
+    baudrate=9600,
+    parity=serial.PARITY_ODD,
+    stopbits=serial.STOPBITS_TWO,
+    bytesize=serial.SEVENBITS,
+	timeout=0
+)
+
+while True:
+    ser.write("\r\nSay something:".encode())
+    #rcv = ser.read(10)
+    #ser.write(("\r\nYou sent:" + repr(rcv)).encode())
+    break
+	
+	
+def uart_send(flag,value):
+    if (flag=='m'):
+	    ser.write(("m"+str(value)).encode())
+    elif (flag=='s'):
+	    ser.write(("s"+str(value)).encode())
+    else:
+        return 1
+    return 0
+	
+def uart_receive_ultrasound():
+    return ser.read(ser.in_wainting())
+	
+#no response from the arduino because it's hard to handle
+#it properly without slowing the programm
+
+#no interrupts for the ultrasound yet, just a one-time call
