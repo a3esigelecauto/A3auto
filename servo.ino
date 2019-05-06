1d954bf35aa338ef0ed9fb6268036b2194ff2d99
Marlin5991 <48325440+Marlin5991@users.noreply.github.com>
2019-05-06 11:20:43 +0200

    Add files via upload

---
+++ servo.ino
@@ -0,0 +1,111 @@
+#include <Servo.h> 
+ 
+
+Servo myservo;     
+
+Servo moteur;
+
+char info;        
+
+char info1;       
+
+void setup(){
+
+
+  myservo.attach(10);        // relier le servo au pin9
+
+  moteur.attach(9);  // relier le moteur au pin10
+
+  Serial.begin(250000);      // pour baud-rate
+
+  moteur.write(1500);
+
+  delay(4000);            //temps d'initialisation
+
+}
+
+
+
+void loop(){
+
+
+
+if (Serial.available()) {
+
+  info=Serial.read();
+
+   if(info=='i')
+
+{ moteur.writeMicroseconds(1500);
+
+ 
+
+}
+
+  else if(info=='m')
+
+{ moteur.writeMicroseconds(1500);
+
+ myservo.write(135);
+
+}
+
+
+
+else if(info=='f')
+
+{
+
+  moteur.writeMicroseconds(1500);
+
+}
+
+
+
+else if (info=='q')
+
+{
+
+  myservo.write(135); 
+
+}
+
+else if(info=='d')
+
+{
+
+   myservo.write(45);
+
+}
+
+else if(info=='c')
+
+{
+
+  myservo.write(90);
+
+}
+
+
+
+else if(info=='z')
+
+{
+
+  moteur.writeMicroseconds(1595);
+
+
+
+}
+
+else
+
+{ 
+
+  moteur.write(1350);
+
+}
+
+}
+
+}
